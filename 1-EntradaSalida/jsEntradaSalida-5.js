b60165dbd2aa9889100f8344a2de691478124833
SamyPetru <52511624+SamyPetru@users.noreply.github.com>
2019-07-03 22:16:52 -0300

    Ultima modificacion
    1 al 5

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,13 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+    var nombre;
+    var edad; 
+    nombre=prompt("Ingrese su nombre");
+    edad=prompt("Ingrese su edad");
+    alert("el nombre es "+nombre+"la edad es "+edad);
+   
+
+
 }
 
